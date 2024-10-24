7f591ff21f01619f6f24134f2544c59b8d641ef1
Yaroslav V. Derkach <Luxors@mail.com>
2024-10-25 00:05:48 +0300

    feat: accordion

--- scripts/common.js
+++ scripts/common.js
@@ -1,10 +1,12 @@
 import introSlider from "./intro-slider.js";
 import { quoteSliderFirst, quoteSliderSecond } from "./quote-sliders.js";
+import { initAccordion } from './accordion.js';
 
 window.addEventListener('DOMContentLoaded', () => {
 	'use srrict';
 
   introSlider,
   quoteSliderFirst,
-  quoteSliderSecond
+  quoteSliderSecond,
+  initAccordion()
 });
